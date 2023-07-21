664078ad5c50803b657a0133d704890b701ad696
gustav0naya <gea25@cornell.edu>
2023-07-20 22:01:56 -0700

    Updated css file, modified color scheme and sizing of charts

--- script.js
+++ script.js
@@ -11,27 +11,15 @@ var margin = { top: 50, right: 50, bottom: 50, left: 150 };
 var width = 800;
 var height = 400;
 
-var svg;
+var svg = d3.select("#chart");
 var tooltip;
 
 function clearChart() {
     d3.select("#chart").select("svg").remove();
     tooltip.style("visibility", "hidden").text("");
     d3.select("body").style('cursor', 'default');
-    svg = d3.select("#chart")
-    .append("svg") // Change the "svg" element to properly set the width and height
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 }
 
-  svg = d3.select("#chart")
-    .append("svg") // Change the "svg" element to properly set the width and height
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   tooltip = d3.select("body")
     .append("div")
@@ -50,10 +38,18 @@ function clearChart() {
 
         d3.select("#narrative_text").text(`
         This narrative overviews the number of injuries throughout the history of the National Basketball Association
-        (NBA). This drill-down narrative starts with the below displayed line chart showcasing hte total number of player
+        (NBA). This drill-down narrative starts with the below displayed line chart showcasing the total number of player
         per year. There appears to be a vast increase in injuries starting in the 1990s, and a closer look at these injuries
-        may provide further details. 
+        may provide further details. Please click on the datapoint on a year to explore further.
         `);
+
+        svg = d3.select("#chart")
+            .append("svg") // Change the "svg" element to properly set the width and height
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .style("cursor", "pointer")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         var dataByYear = d3.nest()
             .key(function (d) {
@@ -102,7 +98,8 @@ function clearChart() {
             .attr("cy", function (d) {
                 return y(d.value);
             })
-            .attr("r", 5);
+            .attr("r", 5)
+            .style("fill", "maroon");
 
         circles
             .on("mouseover", showTooltip)
@@ -143,6 +140,18 @@ function clearChart() {
             .attr("text-anchor", "middle")
             .text("Number of injuries");
 
+        svg.append("text")
+            .attr("class", "title")
+            .attr("x", width / 2)
+            .attr("y", -margin.top / 2)
+            .attr("text-anchor", "middle")
+            .attr("font-weight", "bold")
+            .attr("font-size", 20)
+            .text("NBA Injuries from 1951-2023");
+
+    })
+      .catch(function(error){
+        console.log("Error detected in Overview code", error);
     })
   }
 
@@ -177,12 +186,25 @@ function clearChart() {
         Drilling down, the selected year displays the share of the total injuries per team in the NBA. 
         The tree map visually breaks down the injury share to show which teams had the greatest share of injuries
         in the selected year. Each team has their number of injuries displayed next to their team name in parentheses.
+        Please click on a team to see the injury breakdown.
         `);
 
       data = data.filter(function (el) {
           return (formatYear(parseDate(el.Date)) == Year);
       });
-      console.log(data[0]["Team"]);
+
+      var margin = { top: 30, right: 250, bottom: 140, left: 60 };
+      var width = d3.select('#chart').node().getBoundingClientRect().width - margin.left - margin.right;
+      var height = d3.select('#chart').node().getBoundingClientRect().height - margin.top - margin.bottom;
+
+
+      svg = d3.select("#chart")
+        .append("svg") // Change the "svg" element to properly set the width and height
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .style("cursor", "pointer")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       // Create a nested data structure with the count of each team
       var nestedData = d3.nest()
@@ -209,14 +231,13 @@ function clearChart() {
         .append("rect")
         .attr("x", function(d) { return d.x0; })
         .attr("y", function(d) { return d.y0; })
-        .attr("width", function(d) { return d.x1 - d.x0; })
-        .attr("height", function(d) { return d.y1 - d.y0; })
+        .attr("width", function(d) { return Math.max(0, d.x1 - d.x0 - 1); }) // subtract 1 for padding
+        .attr("height", function(d) { return Math.max(0, d.y1 - d.y0 - 1); }) // subtract 1 for padding
         .style("fill", "navy")
         .style("stroke", "black")
         .style("stroke-width", 1)
         .on("click", function(d) {
         // Handle the click event for each cell
-          console.log("Clicked on cell:", d.data.key);
           clearChart();
           selectedTeamChart(d.data.key, Year);
           currentTeam = d.data.key;
@@ -244,12 +265,14 @@ function clearChart() {
         .attr("x", width / 2)
         .attr("y", -margin.top / 2)
         .attr("text-anchor", "middle")
+        .attr("font-weight", "bold")
+        .attr("font-size", 20)
         .text("Injuries by Team in " + Year);
 
 
     })
     .catch(function(error){
-            console.log("Error detected in Year code");
+            console.log("Error detected in treemap code", error);
     })
   }
 
@@ -270,7 +293,6 @@ function clearChart() {
         data = data.filter(function (el) {
             return (formatYear(parseDate(el.Date)) == Year && el.Team == Team);
         });
-        console.log(data[1]["Relinquished"]);
 
         // Create a nested data structure with the count of each 'Notes' value
       var nestedData = d3.nest()
@@ -284,11 +306,12 @@ function clearChart() {
       });
 
       // Set up the SVG container and dimensions for the bar chart
-      var margin = { top: 20, right: 20, bottom: 60, left: 60 };
-      var width = 800 - margin.left - margin.right;
-      var height = 400 - margin.top - margin.bottom;
+      var margin = { top: 30, right: 20, bottom: 450, left: 140 };
+      var width = d3.select('#chart').node().getBoundingClientRect().width - margin.left - margin.right;
+      var height = d3.select('#chart').node().getBoundingClientRect().height - margin.top - margin.bottom;
 
-      svg
+      var g = d3.select("#chart")
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -300,12 +323,19 @@ function clearChart() {
         .range([0, width])
         .padding(0.1);
 
-      var y = d3.scaleLinear()
-        .domain([0, d3.max(nestedData, function(d) { return d.value; })])
-        .range([height, 0]);
+        var y;
+        if (nestedData.length === 1) {
+          y = d3.scaleLinear()
+            .domain([0, nestedData[0].value + 1]) // Add 1 or some fraction of the value to the maximum value in the domain for a single data point
+            .range([height, 0]);
+        } else {
+          y = d3.scaleLinear()
+            .domain([0, d3.max(nestedData, function(d) { return d.value; })])
+            .range([height, 0]);
+        }
 
       // Create the bars of the bar chart
-      svg.selectAll(".bar")
+      g.selectAll(".bar")
         .data(nestedData)
         .enter().append("rect")
         .attr("class", "bar")
@@ -313,6 +343,7 @@ function clearChart() {
         .attr("y", function(d) { return y(d.value); })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d.value); })
+        .style("fill", "maroon")
         .on("mouseover", function(d) {
           // Show tooltip with 'Relinquished' value on mouseover
           tooltip.text(d.key + ": " + d.value + " (" + getRelinquishedValues(data, d.key).join(", ") + ")");
@@ -328,7 +359,7 @@ function clearChart() {
         });
 
       // Create the x-axis
-      svg.append("g")
+      g.append("g")
         .attr("class", "x-axis")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
@@ -339,33 +370,35 @@ function clearChart() {
         .attr("dx", "-0.8em");
 
       // Create the y-axis
-      svg.append("g")
+      g.append("g")
         .attr("class", "y-axis")
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y).ticks(3).tickFormat(d3.format(".0f")));
 
       // Create the x-axis label
-        svg.append("text")
+        g.append("text")
           .attr("class", "x-axis-label")
           .attr("x", width / 2)
-          .attr("y", height + margin.top + 40)
+          .attr("y", height + margin.top + 170)
           .attr("text-anchor", "middle")
           .text("Injury Reason");
 
         // Create the y-axis label
-        svg.append("text")
+        g.append("text")
           .attr("class", "y-axis-label")
           .attr("transform", "rotate(-90)")
-          .attr("x", -height / 2)
-          .attr("y", -margin.left + 20)
+          .attr("x", -height/2 )
+          .attr("y", -margin.left + 60)
           .attr("text-anchor", "middle")
           .text("Number of injuries");
 
       // Create the title
-        svg.append("text")
+        g.append("text")
             .attr("class", "chart-title")
             .attr("x", width / 2)
             .attr("y", -margin.top / 2)
             .attr("text-anchor", "middle")
+            .attr("font-weight", "bold")
+            .attr("font-size", 20)
             .text("Type of Injuries on the " + Team + " in " + Year);
 
       // Create the tooltip element
@@ -389,34 +422,107 @@ function clearChart() {
 
     })
     .catch(function(error){
-            console.log("Error detected in Year code");
+            console.log("Error detected in barchart code", error);
     })
   }
+
+  // Update the SVG width and height to match the window dimensions
+function updateChartSize() {
+  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  svg.attr("width", width).attr("height", height);
+}
+
+window.addEventListener("resize", updateChartSize);
+
 async function init() {
-
-
+  updateChartSize()
   injuryYearsChart();
 
-
-  async function drawScatterPlot() {
-    var data = await d3.csv("NBAPlayerInjury.csv");
-
-    var x = d3.scaleLog().base(10).domain([10, 150]).range([0, 200]);
-    var y = d3.scaleLog().base(10).domain([10, 150]).range([200, 0]);
-
-    var circles = svg.selectAll("circle")
-      .data(data)
-      .enter().append("circle")
-      .attr("cx", function (d) { return x(Math.random() * 140 + 10); })
-      .attr("cy", function (d) { return y(Math.random() * 140 + 10); })
-      .attr("r", 2);
-
-    circles
-      .on("mouseover", showTooltip)
-      .on("mousemove", moveTooltip)
-      .on("mouseout", hideTooltip);
-
-  }
 }
 
 window.addEventListener("DOMContentLoaded", init);
+
+//annotations
+
+// const annotations = [
+//         //moved recession annotation to front of array so it's behind
+//         //the Macbook Air annotation
+//         {
+//           note: {
+//             title: "Spike in injuries",
+//             lineType: "none",
+//             align: "middle",
+//             wrap: 150 //custom text wrapping
+//           },
+//           subject: {
+//             height: height - margin.top - margin.bottom,
+//             width: x(new Date("6/1/2009")) - x(new Date("12/1/2007"))
+//           },
+//           type: d3.annotationCalloutRect,
+//           y: margin.top,
+//           disable: ["connector"], // doesn't draw the connector
+//           //can pass "subject" "note" and "connector" as valid options
+//           dx: (x(new Date("12/1/2021")) - x(new Date("12/1/1999")))/2,
+//           data: { x: "12/1/1999"}
+//         },
+//         {
+//           subject: {
+//             text: "A",
+//             x: "right" //badges have an x of "left" or "right"
+//           },
+//           data: { x: "1/10/1990", y: 80.9}
+//         },
+//         {
+//           subject: { text: "B" },
+//           data: { x: "6/29/1998", y: 122}
+//         },
+//         {
+//           subject: {
+//             text: "C",
+//             y: "bottom",
+//             x: "right"
+//           },
+//           data: { x: "4/3/2021", y: 238}
+//         }]
+//
+// const type = d3.annotationCustomType(
+//     d3.annotationBadge,
+//     {"subject":{"radius": 10 }}
+//   )
+//
+// const makeAnnotations = d3.annotation()
+//     .type(type)
+//     .accessors({
+//       x: function(d){ return x(new Date(d.x))},
+//       y: function(d){ return y(d.y) }
+//     })
+//     .annotations(annotations)
+//
+//
+// //annotations for legend
+// const annotationsLegend = [{
+//       note: { label: "Injuries remain relatively flat until 1990" },
+//       subject: { text: "A" }
+//     },
+//     {
+//       note: { label: "Injuries start rising" },
+//       subject: { text: "B" }
+//     },
+//     {
+//       note: { label: "Injuries Peak" },
+//       subject: { text: "C" }
+//     }
+//     ].map(function(d, i){
+//       d.x = margin.left + i*200
+//       d.y = 415
+//       d.subject.x = "right"
+//       d.subject.y = "bottom"
+//       d.subject.radius = 10
+//       return d
+//     })
+//
+// const makeLegendAnnotations = d3.annotation()
+//     .type(d3.annotationBadge)
+//     .annotations(annotationsLegend)
